89010a1d4c4bcacdf29ae6327fbd752f878ba188
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2024-06-24 21:48:30 +0200

    added output converter to list

--- src/main/java/com/example/demo/outputparser/controller/SongsController.java
+++ src/main/java/com/example/demo/outputparser/controller/SongsController.java
@@ -4,11 +4,17 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.converter.StructuredOutputConverter;
+import org.springframework.ai.parser.ListOutputParser;
+import org.springframework.ai.parser.OutputParser;
+import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -30,5 +36,18 @@ public class SongsController {
         Prompt prompt = promptTemplate.create();
         ChatResponse response = chatClient.prompt(prompt).call().chatResponse();
         return response.getResult().getOutput().getContent();
+    }
+
+    @GetMapping("/v2/popular")
+    public List<String> getSongsByArtistV2(@RequestParam(value = "artist", defaultValue = "Taylor Swift") String artist) {
+        String message = """
+                Please give me a list of top 10 songs for the artist {artist}. If you don't know the answer, just say "I don't know."
+                {format}
+                """;
+        ListOutputConverter listOutputConverter = new ListOutputConverter(new DefaultConversionService());
+        PromptTemplate promptTemplate = new PromptTemplate(message, Map.of("artist", artist, "format", listOutputConverter.getFormat()));
+        Prompt prompt = promptTemplate.create();
+        ChatResponse response = chatClient.prompt(prompt).call().chatResponse();
+        return listOutputConverter.convert(response.getResult().getOutput().getContent());
     }
 }
