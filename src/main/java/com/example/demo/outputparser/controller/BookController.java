def003106628d4e09a26d3eeb5b8e8fa2314b216
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2024-06-25 20:17:09 +0200

    added output bean converter

--- src/main/java/com/example/demo/outputparser/controller/BookController.java
+++ src/main/java/com/example/demo/outputparser/controller/BookController.java
@@ -1,15 +1,13 @@
 package com.example.demo.outputparser.controller;
 
+import com.example.demo.outputparser.controller.entity.Author;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.model.Generation;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.converter.BeanOutputConverter;
 import org.springframework.ai.converter.MapOutputConverter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -35,5 +33,19 @@ public class BookController {
         Prompt prompt = promptTemplate.create();
         Generation generation = chatClient.prompt(prompt).call().chatResponse().getResult();
         return mapOutputConverter.convert(generation.getOutput().getContent());
+    }
+
+    @GetMapping("/byAuthor")
+    public Author getBookByAuthor(@RequestParam(value = "author", defaultValue = "Ken Kousen") String author) {
+        String message = """
+                Generate a list of book written by the author {author}. If you aren't positive that a book belong to this author please don't include it.
+                {format}
+                """;
+        BeanOutputConverter<Author> outputConverter = new BeanOutputConverter<>(Author.class);
+        String format = outputConverter.getFormat();
+        PromptTemplate promptTemplate = new PromptTemplate(message, Map.of("author", author, "format", format));
+        Prompt prompt = promptTemplate.create();
+        Generation generation = chatClient.prompt(prompt).call().chatResponse().getResult();
+        return outputConverter.convert(generation.getOutput().getContent());
     }
 }
---
+++ src/main/java/com/example/demo/outputparser/controller/entity/Author.java
@@ -0,0 +1,6 @@
+package com.example.demo.outputparser.controller.entity;
+
+import java.util.List;
+
+public record Author(String author, List<String> books) {
+}
